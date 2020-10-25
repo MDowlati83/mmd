3ae41e742c6dbb7f935a8ec272404df350072342
MDowlati83 <mashooq.dowlati@gmail.com>
2020-10-25 14:33:37 -0400

    maven project

---
+++ com.mmd.project/src/main/java/com/mmd/project/com/mmd/project/App.java
@@ -0,0 +1,13 @@
+package com.mmd.project.com.mmd.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com.mmd.project/src/main/java/com/mmd/project/com/mmd/project/Test.java
@@ -0,0 +1,11 @@
+package com.mmd.project.com.mmd.project;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Just Practising my push and pull skills");
+	}
+
+}
